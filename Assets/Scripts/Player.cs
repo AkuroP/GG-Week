e1f475ce6e7b7bd5afcb7cb2e137a56bb943670e
AkuroP <thomas.kang2002@gmail.com>
2022-04-26 09:45:40 +0200

    Player Jump + Movement
    + Added Input for player jump and movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,23 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+    [Header("Player Move")]
+    public float playerSpeed;
+    public float playerJumpForce;
+    private float horizontalMove;
+    [SerializeField]
+    private int jumpCount;
+
+    [Header("Dimension Switch")]
+    [Header("True = Life; False = Death")]
     public GameObject lifeDimension;
     public GameObject deathDimension;
-
-    [Header("True = Life; False = Death")]
     [SerializeField]
     private bool lifeOrDeath;
+
+
     // Start is called before the first frame update
     
     private void Awake()
@@ -18,10 +29,14 @@ public class Player : MonoBehaviour
         lifeOrDeath = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMove * playerSpeed, rb.velocity.y);
     }
 
     public void Switch(InputAction.CallbackContext context)
@@ -40,6 +55,25 @@ public class Player : MonoBehaviour
                 deathDimension.SetActive(false);
                 lifeOrDeath = true;
             }
+        }
+    }
+
+    public void PlayerMovement(InputAction.CallbackContext context)
+    {
+        horizontalMove = context.ReadValue<Vector2>().x;
+    }
+
+    public void PlayerJump(InputAction.CallbackContext context)
+    {
+        if(context.performed && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
+            jumpCount--;
+        }
+
+        if(context.canceled)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
         }
     }
 }
