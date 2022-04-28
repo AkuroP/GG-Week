b9afd0f8edbb64975602b4d7dcf75475a14a3b85
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2022-04-28 12:15:19 +0200

    DeathZone

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    public GameObject portail;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+            StartCoroutine(Teleport());
+            PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(20);
+
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(0.2f);
+        player.transform.position = new Vector2 (portail.transform.position.x, portail.transform.position.y);
+        
+    }
+
+    
+}
