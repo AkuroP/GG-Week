8b3aa2adb93e7b67dd5af052b719137f7891afdb
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2022-04-28 12:19:35 +0200

    UpdateFor3D

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -20,7 +20,7 @@ public class DeathZone : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Player")
         {
